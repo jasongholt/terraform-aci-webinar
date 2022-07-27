e264774f759955a54eceba3dd2e6a741fc417689
Jason Holt <95232586+jasongholt@users.noreply.github.com>
2022-07-27 15:24:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,12 +36,6 @@ module "vlan2" {
   unicast_route   = "yes"
 }
 
-module "vlan3" {
-  source = "app.terraform.io/jasongholt-demos/epg-vlan/aci"
-  version         = ">=0.0.1"
-  tenant_name     = "ACMEC-tenant"
-  gateway_address = "10.10.2.1/24"
-  unicast_route   = "yes"
-}
+
 
 
